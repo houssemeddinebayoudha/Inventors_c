24541e1bce2b41dada362eb058430cecad5bcc2f
houssemeddinebayoudha <houssemeddine.bayoudha@esprit.tn>
2021-01-06 08:59:42 +0100

    v5

--- equitest/src/callbacks.h
+++ equitest/src/callbacks.h
@@ -83,3 +83,21 @@ on_retoursuccess_clicked               (GtkWidget       *button,
 void
 on_errorredo_clicked                   (GtkButton       *button,
                                         gpointer         user_data);
+
+void
+on_radiobutton5_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_radiobutton6_toggled                (GtkToggleButton *togglebutton,
+                                        gpointer         user_data);
+
+void
+on_sup_clicked                         (GtkWidget       *button,
+                                        gpointer         user_data);
+
+void
+on_modif_clicked                       (GtkWidget       *button,
+                                        gpointer         user_data);
+
+
